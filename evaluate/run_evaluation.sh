48bf1cff9fbbcfee5b7c2c389a4587ac59e80a6d
Ricardo <richardignacio888@gmail.com>
2024-05-08 13:14:25 +0200

    pruebas evaluación para ver si el script proporcionado para computar ADD funciona

---
+++ evaluate/run_evaluation.sh
@@ -0,0 +1,3 @@
+# Evaluation
+
+python evaluate.py --data_prediction ../output/inference_results/connector_black/corrected_1_2_vimba/vicon-FixedPosExp-vimba_renamed/connector_black_vicon_exp_position1/net_epoch_40 --data ../sample_data 
